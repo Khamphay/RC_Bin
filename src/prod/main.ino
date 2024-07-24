6c153eb747b3511950f8e5d833ba60fbe2383f0c
Khamphay <txammuas1@gmail.com>
2024-07-24 16:34:26 +0700

    update

--- src/prod/main.ino
+++ src/prod/main.ino
@@ -5,24 +5,33 @@
 
 #define LDT 30
 #define LSCK 31
+#define BTN 32
 
-AF_DCMotor mymotor(1);
+AF_DCMotor mymotor1(1);
+AF_DCMotor mymotor2(4);
 Servo myservo; // create Servo object to control a servo
 HX711 myscale;
 
 const float calibration_factor = 123790;
 int sdg = 0;
+float weight = 0;
+unsigned fQR = 0;
 
 void setup()
 {
   Serial.begin(115200); // Serial monitor
   // Serial1.begin(9600);  // Serial communication with LILYGO T-Call
 
+  pinMode(BTN, INPUT_PULLUP);
+
   myservo.attach(10);
   myservo.write(sdg);
 
-  mymotor.setSpeed(200);
-  mymotor.run(RELEASE);
+  mymotor1.setSpeed(120);
+  mymotor1.run(RELEASE);
+
+  mymotor2.setSpeed(500);
+  mymotor2.run(RELEASE);
 
   delay(2000);
   Serial.println("Initailing...");
@@ -57,28 +66,55 @@ void loop()
   //   delay(10);
   // }
 
-  if (sdg <= 0)
+  // if (sdg <= 0)
+  // {
+  //     sdg = 80;
+  //     myservo.write(sdg);
+  //     delay(1000);
+  // }
+
+  // if (sdg <= 80)
+  // {
+  //   sdg = 0;
+  //   myservo.write(sdg);
+  //   delay(1000);
+  // }
+
+  weight = myscale.get_units();
+  int btnP = digitalRead(BTN);
+  Serial.println(weight);
+  if (btnP == 1 && weight > 0.005 && weight <= 0.2)
+  {
+    mymotor1.run(BACKWARD);
+    mymotor2.run(RELEASE);
+  }
+  else if (btnP == 0 && weight > 0.0009 && weight <= 0.2)
+  {
+    mymotor1.run(RELEASE);
+    mymotor2.run(BACKWARD);
+    sdg = 80;
+    myservo.write(sdg);
+    delay(6000);
+    // mymotor2.run(RELEASE);
+    // sdg = 0;
+    // myservo.write(sdg);
+  }
+  else if (weight > 0.2 && btnP == 1)
   {
     sdg = 80;
     myservo.write(sdg);
-    delay(10000);
+    mymotor1.run(RELEASE);
+    mymotor2.run(FORWARD);
   }
-
-  if (sdg <= 80)
+  else // if (weight <= 0)
   {
-    sdg = 0;
-    myservo.write(sdg);
-    delay(1000);
-  }
-
-  float weight = myscale.get_units();
-  if (weight > 0 && weight <= 0.6)
-  {
-    mymotor.run(FORWARD);
-  }
-  else if (weight > 1)
-  {
-    mymotor.run(BACKWARD);
+    mymotor1.run(RELEASE);
+    mymotor2.run(RELEASE);
+    if (sdg == 80)
+    {
+      sdg = 0;
+      myservo.write(sdg);
+    }
   }
 
   delay(1500);
